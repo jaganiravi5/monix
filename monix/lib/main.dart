7b6e9c301a724a24a533451393259d5b28dd7435
Navin Panchal <navinp@zignuts.com>
2024-06-20 17:49:43 +0530

    Added firebase

--- monix/lib/main.dart
+++ monix/lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:common/common.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:monix/router/router.dart';
 import 'package:network/network.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   MobileAds.instance.initialize();
+
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
